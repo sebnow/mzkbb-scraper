0e26e3e586b032ab1c18e0ec6d06e200a9bc47ac
Sebastian Nowicki <sebnow@gmail.com>
2012-01-30 20:42:34 +0800

    Refactory and unit test

--- mzkbb-scraper.py
+++ mzkbb-scraper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# coding=utf8
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 from urllib2 import urlopen
 from urlparse import urljoin, urlparse, urlunparse
@@ -68,13 +69,9 @@ def get_stops(page):
 			"name": strip_stop.sub("", el('font')[0].contents[0]),
 			"id": el['href'][2:-6],
 		}
-		details = get_stop_details(stop['name'])
-		stop["longitude"] = details["longitude"]
-		stop["lattitude"] = details["lattitude"]
 		yield stop
 
-def get_stop_details(name):
-	gps_re = re.compile("(\d+).(\d+)'(\d+)\"\s*(\w)")
+def get_stop_detail_page(name):
 	qstring = cgi.escape(name).encode('ascii', 'xmlcharrefreplace').replace(" ", "+")
 	url = urljoin(LB_URL, "/m/szukaj?q=" + qstring)
 	log.debug("Retrieving GPS details from " + url)
@@ -97,48 +94,69 @@ def get_stop_details(name):
 		url = platforms[0].parent.parent['href']
 		log.debug("Found stop detail page: " + url)
 		page = urlopen(url)
-		soup = BeautifulSoup(page)
 	
-	log.debug(soup.findAll('div', id="peron-info"))
+def extract_stop_gps(page):
+	""" Extract GPS information from the specified *page*.
+
+	Returns a dictionary with the WGS84 *longitude* and *lattitude*.
+
+	>>> import StringIO
+	>>> page = StringIO.StringIO()
+	>>> page.write('<div id="peron-info">')
+	>>> page.write('''<p>GPS: <strong>49°50'06" N, 19°00'32" E</strong></p>''')
+	>>> page.write('</div>')
+	>>> page.seek(0)
+	>>> extract_stop_gps(page)
+	{'longitude': 49.835, 'lattitude': 19.00888888888889}
+	"""
+	soup = BeautifulSoup(page)
 	info = soup.findAll('div', id="peron-info")[0]
 
 	gps = info.findAll('p')[0].findAll('strong')[0].contents[0].strip()
-	log.debug("GPS: " + repr(gps))
 	gps = gps.split(', ', 2)[:2]
 
-	log.debug("matching lon: " + repr(gps[0]))
-	log.debug(gps_re.match(gps[0]))
-	[d, m, s] = gps_re.match(gps[0]).groups()[:3]
-	lon = dms_to_wgs84(int(d), int(m), int(s))
-	log.debug("long: " + str(lon))
+	gps_re = re.compile("(\d+).(\d+)'(\d+)\"\s*(\w)")
+	[d, m, s, di] = gps_re.match(gps[0]).groups()[:4]
+	lon = dms_to_decimal(int(d), int(m), int(s), di)
 
-	log.debug("matching lon: " + repr(gps[1]))
-	[d, m, s] = gps_re.match(gps[1]).groups()[:3]
-	lat = dms_to_wgs84(int(d), int(m), int(s))
-	log.debug("lat: " + str(lat))
+	[d, m, s, di] = gps_re.match(gps[1]).groups()[:4]
+	lat = dms_to_decimal(int(d), int(m), int(s), di)
 
 	return {
 		"longitude": lon,
 		"lattitude": lat,
 	}
 
+def dms_to_decimal(degress, minutes, seconds, direction):
+	""" Convert degrees, minutes and seconds into decimal.
 
-def dms_to_wgs84(degress, minutes, seconds):
-	return degress + (minutes / 60.0) + (seconds / 3600.0)
+	>>> round(dms_to_decimal(49, 50, 06, "E"), 4)
+	49.835
+	>>> round(dms_to_decimal(19, 0, 32, "N"), 4)
+	19.0089
+	>>> round(dms_to_decimal(49, 50, 06, "W"), 4)
+	-49.835
+	>>> round(dms_to_decimal(19, 0, 32, "S"), 4)
+	-19.0089
+	"""
+	direction = direction.upper()
+	sign = 1
+	if direction == 'W' or direction == 'S':
+		sign = -1
+	return (degress + (minutes / 60.0) + (seconds / 3600.0)) * sign
 
-if __name__ == "__main__":
+def scrape_stops():
 	locations = get_locations(urlopen(MZKBB_LOCATION_URL))
 	log.debug("Locations: " + repr(locations))
-	stops = []
 	for location in locations:
-		log.info("Retrieving stops for "+location['name'])
-		log.debug("url: " + location['url'])
-		page = urlopen(location['url'])
-		stops.extend(get_stops(page))
-	log.debug("Stops: " + repr(stops))
-	log.info("Writing stops.txt")
-	fh = open("stops.txt", 'wb')
-	stops_csv = csv.DictWriter(fh, ['id', 'name', 'name', 'description', 'lattitude', 'longitude', 'zone_id', 'url'])
-	[stops_csv.writerow(s) for s in stops]
-	fh.close()
+		location_page = urlopen(location['url'])
+		for stop in get_stops(location_page):
+			yield stop
+
+
+if __name__ == "__main__":
+	with open("stops.txt", "wb") as fh:
+		log.info("Writing stops.txt")
+		stops_csv = csv.DictWriter(fh, ['id', 'name', 'name', 'description', 'lattitude', 'longitude', 'zone_id', 'url'])
+		[stops_csv.writerow(s) for s in scrape_stops()]
 
