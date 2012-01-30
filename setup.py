e145ca1c6a7c47f66e3bf481a277744d39d8195a
Sebastian Nowicki <sebnow@gmail.com>
2012-01-30 22:40:36 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+	name = "mzkbb-scraper",
+	version = "0.1.0",
+	author = "Sebastian Nowicki"
+	author_email = "sebnow@gmail.com",
+	description = "Scraper for MZK Bielsko-Biala GTFS data",
+	license = "MIT",
+	keywords = "web scraper",
+)
+
